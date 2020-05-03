ac1738b1d58de536ed083b4a47a1a8087a6a4689
Marko Tasic <mtasic85@gmail.com>
2020-05-03 20:17:38 +0200

    cleanup

--- main.c
+++ main.c
@@ -143,56 +143,6 @@ int exec_js_code(uv_handle_t *uv_handle, char *js_code) {
     duk_eval_string(ctx, "const lodash = require('./lodash.min');");
     duk_eval_string(ctx, "console.log(lodash.range(10));");
     duk_eval_string(ctx, "console.log(Number.isInteger(1));");
-
-    // duk_eval_string(ctx,
-    //     "var __BRYTHON__ = {};"
-    //     "__BRYTHON__.brython = null;"
-    //     "__BRYTHON__.isNone = true;"
-    // );
-    
-    // duk_eval_string(ctx, "$compile_python = function(module_contents,module) {};");
-    // duk_eval_string(ctx,
-    //     "global = {};"
-    //     "document = {};"
-    //     "document.getElementsByTagName = function () { return [{src: ''}] };"
-    //     "window = {};"
-    //     "window.location = {href: ''};"
-    //     "window.navigator = {};"
-    //     "window.confirm = function () { return true; };"
-    //     "window.console = console;"
-    //     "document.$py_src = {};"
-    //     "document.$debug = 0;"
-    
-    //     "self = {};"
-    //     "__BRYTHON__ = {};"
-    //     "__BRYTHON__.$py_module_path = {};"
-    //     "__BRYTHON__.$py_module_path['__main__'] = './';"
-    //     "__BRYTHON__.$py_module_alias = {};"
-    //     "__BRYTHON__.$py_next_hash = -Math.pow(2,53);"
-    //     "__BRYTHON__.exception_stack = [];"
-    //     "__BRYTHON__.scope = {};"
-    //     "__BRYTHON__.modules = {};"
-    //     "__BRYTHON__.isNode = true;"
-    
-    //     "__BRYTHON__.$py_module_path = __BRYTHON__.$py_module_path || {};"
-    //     "__BRYTHON__.$py_module_alias = __BRYTHON__.$py_module_alias || {};"
-    //     "__BRYTHON__.exception_stack = __BRYTHON__.exception_stack || [];"
-    //     "__BRYTHON__.scope = __BRYTHON__.scope || {};"
-    //     "__BRYTHON__.imported = __BRYTHON__.imported || {};"
-    //     "__BRYTHON__.modules = __BRYTHON__.modules || {};"
-    //     // "__BRYTHON__.compile_python = $compile_python;"
-    
-    //     "__BRYTHON__.debug = 0;"
-    //     "__BRYTHON__.$options = {};"
-    //     "__BRYTHON__.$options.debug = 0;"
-    // );
-    // duk_eval_string(ctx, "const py2js = require('./brython/www/src/py2js');");
-    // duk_eval_string(ctx, "console.log(py2js);");
-    // duk_eval_string(ctx, "console.log(__BRYTHON__);");
-    // duk_eval_string(ctx, "var root = __BRYTHON__.py2js('print(123)', '__main__');");
-    // duk_eval_string(ctx, "console.log(root);");
-    // duk_eval_string(ctx, "const brython = require('./brython_dist_babel');");
-    // duk_eval_string(ctx, "console.log(brython);");
     
     /* pop eval result */
     duk_pop(ctx);
